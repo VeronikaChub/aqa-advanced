729836d44a1384c4738ccc4c655cc808a02c5382
Veronika <Chubai9728@gmail.com>
2026-07-03 18:02:31 +0300

    home_work4

---
+++ jsBasics/Home_work4/task3.js
@@ -0,0 +1,11 @@
+const tablenumber = 2;
+
+for (let row = 1; row <= 10; row++) {
+    console.log(`${tablenumber} * ${row} = ${tablenumber * row}`);
+}
+
+let row = 1;
+while (row <= 10) {
+    console.log(`${tablenumber} * ${row} = ${tablenumber * row}`);
+    row++;
+}
