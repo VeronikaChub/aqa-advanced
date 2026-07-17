ea1bfcacca2ff3e86dbe2c6bd33b704e3cbe7be1
Veronika <Chubai9728@gmail.com>
2026-07-17 18:49:14 +0300

    home_work7

---
+++ jsBasics/Home_work7/task3.js
@@ -0,0 +1,3 @@
+const array = [10, 20, 30, 40, 50];
+const sum = array.reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(sum);
---
+++ jsBasics/Home_work7/task4.js
@@ -0,0 +1,3 @@
+const array = [1, 3, 10, 4, 20, 54, 30, 41, 59];
+const numberSelcttion = array.filter((number) => number % 2 === 0);
+console.log(numberSelcttion);
