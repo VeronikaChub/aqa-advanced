df9a3e0f50c31f3e6a28929415fc26c6787a373c
Veronika <Chubai9728@gmail.com>
2026-07-22 19:04:34 +0300

    home_work8

---
+++ jsBasics/Home_work8/task2.js
@@ -0,0 +1,11 @@
+const book = {
+    title: "Way",
+    author: "Jon Feedr",
+    year: 1998
+}
+
+const { title, author, year } = book
+
+console.log(title);
+console.log(author);
+console.log(year);
